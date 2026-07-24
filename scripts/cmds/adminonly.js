12d5f61846ade1e83668508c0b0974d5da9a5d61
fareskouachi804-sys <fareskouachi804@gmail.com>
2026-07-24 16:42:26 +0100

    Update adminonly.js

--- scripts/cmds/adminonly.js
+++ scripts/cmds/adminonly.js
@@ -11,11 +11,14 @@ module.exports = {
 		countDown: 5,
 		role: 2,
 		description: {
+			ar: "تشغيل أو إيقاف وضع المسؤولين فقط لاستخدام البوت بكل حب ✨🩵",
 			vi: "bật/tắt chế độ chỉ admin mới có thể sử dụng bot",
 			en: "turn on/off only admin can use bot"
 		},
 		category: "owner",
 		guide: {
+			ar: "   {pn} [on | off]: تشغيل أو إيقاف وضع المسؤولين فقط لاستخدام البوت 🥺🍓"
+				+ "\n   {pn} noti [on | off]: تشغيل أو إيقاف الإشعار عندما يحاول شخص ليس مسؤولاً استخدام البوت ✨",
 			vi: "   {pn} [on | off]: bật/tắt chế độ chỉ admin mới có thể sử dụng bot"
 				+ "\n   {pn} noti [on | off]: bật/tắt thông báo khi người dùng không phải là admin sử dụng bot",
 			en: "   {pn} [on | off]: turn on/off the mode only admin can use bot"
@@ -24,6 +27,12 @@ module.exports = {
 	},
 
 	langs: {
+		ar: {
+			turnedOn: "يا عمري، تم تشغيل وضع المسؤولين فقط لاستخدام البوت بنجاح ✨🩵",
+			turnedOff: "يا روحي، تم إيقاف وضع المسؤولين وأصبح بإمكان الجميع استخدام البوت 🥺🍓",
+			turnedOnNoti: "يا غالي، تم تفعيل إشعار التنبيه عندما يحاول غير المسؤول استخدام البوت 🩵✨",
+			turnedOffNoti: "يا حياتي، تم إيقاف إشعار التنبيه لغير المسؤولين بنجاح 🥺"
+		},
 		vi: {
 			turnedOn: "Đã bật chế độ chỉ admin mới có thể sử dụng bot",
 			turnedOff: "Đã tắt chế độ chỉ admin mới có thể sử dụng bot",
