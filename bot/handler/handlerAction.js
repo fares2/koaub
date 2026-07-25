b94136b5dbd5ecd9c1b2b09f73b6dab9489895a7
fares2 <tkhtmt005@gmail.com>
2026-07-25 12:40:14 +0100

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -1,8 +1,8 @@
 const createFuncMessage = global.utils.message;
 const handlerCheckDB = require("./handlerCheckData.js");
 
-// استدعاء ملف الرسائل لتفادي خطأ ReferenceError
-const messages = require("./messages.js"); 
+// استدعاء ملف الرسائل بالمسار الصحيح بناءً على هيكلة المشروع
+const messages = require("../../scripts/utils/messages.js"); 
 
 const request = require("request");
 const axios = require("axios");
@@ -82,33 +82,36 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
 				if (event.type == "message_unsend") {
 					let resend = await threadsData.get(event.threadID, "settings.reSend");
 					if (resend == true && event.senderID !== api.getCurrentUserID()) {
-						let umid = global.reSend[event.threadID].findIndex(e => e.messageID === event.messageID);
+						if (global.reSend && global.reSend[event.threadID]) {
+							let umid = global.reSend[event.threadID].findIndex(e => e.messageID === event.messageID);
 
-						if (umid > (-1)) {
-							let nname = await usersData.getName(event.senderID);
-							let attch = [];
-							if (global.reSend[event.threadID][umid].attachments.length > 0) {
-								let cn = 0;
-								for (var abc of global.reSend[event.threadID][umid].attachments) {
-									if (abc.type == "audio") {
-										cn += 1;
-										let pts = `scripts/cmds/tmp/${cn}.mp3`;
-										let res2 = (await axios.get(abc.url, {
-											responseType: "arraybuffer"
-										})).data;
-										fs.writeFileSync(pts, Buffer.from(res2, "utf-8"));
-										attch.push(fs.createReadStream(pts));
-									} else {
-										attch.push(await global.utils.getStreamFromURL(abc.url));
+							if (umid > -1) {
+								let nname = await usersData.getName(event.senderID);
+								let attch = [];
+								if (global.reSend[event.threadID][umid].attachments && global.reSend[event.threadID][umid].attachments.length > 0) {
+									let cn = 0;
+									fs.ensureDirSync("scripts/cmds/tmp");
+									for (var abc of global.reSend[event.threadID][umid].attachments) {
+										if (abc.type == "audio") {
+											cn += 1;
+											let pts = `scripts/cmds/tmp/${cn}.mp3`;
+											let res2 = (await axios.get(abc.url, {
+												responseType: "arraybuffer"
+											})).data;
+											fs.writeFileSync(pts, Buffer.from(res2, "utf-8"));
+											attch.push(fs.createReadStream(pts));
+										} else {
+											attch.push(await global.utils.getStreamFromURL(abc.url));
+										}
 									}
 								}
-							}
 
-							api.sendMessage({
-								body: nname + " removed:\n\n" + global.reSend[event.threadID][umid].body,
-								mentions: [{ id: event.senderID, tag: nname }],
-								attachment: attch
-							}, event.threadID);
+								api.sendMessage({
+									body: nname + " removed:\n\n" + (global.reSend[event.threadID][umid].body || ""),
+									mentions: [{ id: event.senderID, tag: nname }],
+									attachment: attch
+								}, event.threadID);
+							}
 						}
 					}
 				}
@@ -125,15 +128,19 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
 							if (err) return console.log(err);
 						});
 					} else {
-						message.send(":)");
+						message.reply(":)");
 					}
 				}
 				if (event.reaction == "😡") {
 					if (event.senderID == api.getCurrentUserID()) {
 						if (event.userID == "61589591233031") {
-							message.unsend(event.messageID);
+							if (typeof message.unsend === "function") {
+								message.unsend(event.messageID);
+							} else {
+								api.unsendMessage(event.messageID);
+							}
 						} else {
-							message.send(":)");
+							message.reply(":)");
 						}
 					}
 				}
