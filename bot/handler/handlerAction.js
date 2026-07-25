88609488eee187a8f486f39549211638afb58290
fares2 <tkhtmt005@gmail.com>
2026-07-25 11:55:56 +0100

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -4,6 +4,7 @@ const handlerCheckDB = require("./handlerCheckData.js");
 const request = require("request")
 const axios = require("axios")
 const fs = require("fs-extra")
+const messages = require("../../scripts/utils/messages");
 
 
 module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData) => {
@@ -11,6 +12,56 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
 
 	return async function (event) {
 		const message = createFuncMessage(api, event);
+		message.success = (text) =>
+	message.reply(text || messages.random(messages.success));
+
+message.error = (text) =>
+	message.reply(text || messages.random(messages.error));
+
+message.loading = (text) =>
+	message.reply(text || messages.random(messages.loading));
+
+message.noPermission = (text) =>
+	message.reply(text || messages.random(messages.noPermission));
+
+message.botNoPermission = (text) =>
+	message.reply(text || messages.random(messages.botNoPermission));
+
+message.done = (text) =>
+	message.reply(text || messages.random(messages.done));
+
+message.wait = (text) =>
+	message.reply(text || messages.random(messages.wait));
+
+message.welcome = (text) =>
+	message.reply(text || messages.random(messages.welcome));
+
+message.goodbye = (text) =>
+	message.reply(text || messages.random(messages.goodbye));
+
+message.ai = (text) =>
+	message.reply(text || messages.random(messages.ai));
+
+message.download = (text) =>
+	message.reply(text || messages.random(messages.download));
+
+message.upload = (text) =>
+	message.reply(text || messages.random(messages.upload));
+
+message.onlyOwner = (text) =>
+	message.reply(text || messages.random(messages.onlyOwner));
+
+message.onlyAdmin = (text) =>
+	message.reply(text || messages.random(messages.onlyAdmin));
+
+message.onlyGroup = (text) =>
+	message.reply(text || messages.random(messages.onlyGroup));
+
+message.cooldown = (text) =>
+	message.reply(text || messages.random(messages.cooldown));
+
+message.notFound = (text) =>
+	message.reply(text || messages.random(messages.notFound));
 
 		await handlerCheckDB(usersData, threadsData, event);
 		const handlerChat = await handlerEvents(event, message);
